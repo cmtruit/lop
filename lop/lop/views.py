db8360cee426dc004942302202232f2e08552cf3
cmtruit <christruitt@msn.com>
2017-10-28 14:53:09 -0500

    edit

--- lop/lop/views.py
+++ lop/lop/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
-
+from django.conf import settings
 
 def index(request):
     return render(request, 'index.html')
