16ece5b63ea46e1d9a9274877218dfe9fe224e8a
cmtruit <christruitt@msn.com>
2017-10-28 15:26:42 -0500

    edit

--- lop/lop/settings.py
+++ lop/lop/settings.py
@@ -129,5 +129,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATICFILES_DIRS = (
+    os.path.join(SITE_ROOT, '..', 'static'),
+)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
