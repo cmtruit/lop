c3e686df0881da7e5b906cf69fe2a4c12593696d
cmtruit <christruitt@msn.com>
2017-10-28 16:13:21 -0500

    undo

--- lop/lop/urls.py
+++ lop/lop/urls.py
@@ -22,5 +22,4 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
     url(r'^$', views.index, name='index'),
-    url(r'^static/', settings.STATICFILES_DIRS, name='static'),
 ]
