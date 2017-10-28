01138ae04d8f07251a6dc237da1380853e631297
cmtruit <christruitt@msn.com>
2017-10-28 16:11:43 -0500

    edit

--- lop/lop/urls.py
+++ lop/lop/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from . import views
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^static/', STATICFILES_DIRS, name='static'),
 ]
