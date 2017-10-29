2897aea29e5b3d7a394906ea8021d4058eac64c8
cmtruit <christruitt@msn.com>
2017-10-28 19:05:39 -0500

    a

--- lop/lop/settings.py
+++ lop/lop/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-print(STATIC_ROOT)
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+#print(STATIC_ROOT)
 STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'lop/static'), )
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
