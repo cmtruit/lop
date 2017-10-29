38f43d9febddc24aac18cb0853c8631274672919
cmtruit <christruitt@msn.com>
2017-10-29 08:18:33 -0400

    env changes

---
+++ lop/api/views.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render
+from django.conf.urls import url
+from rest_framework_swagger.views import get_swagger_view
+
+api_view = get_swagger_view(title='Lights-Out Patching API')
+
+urlpatterns = [
+    url(r'^$', api_view)
+]
