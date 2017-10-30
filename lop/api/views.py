ff569152be3139d055366d763991f7d48056a810
cmtruit <christruitt@msn.com>
2017-10-29 21:02:04 -0500

    a

--- lop/api/views.py
+++ lop/api/views.py
@@ -5,5 +5,5 @@ from rest_framework_swagger.views import get_swagger_view
 schema_view = get_swagger_view(title='Pastebin API')
 
 urlpatterns = [
-    url(r'^$', schema_view)
+    url(r'^api/', schema_view)
 ]
