076829d15a314b39706979a63fa59d621f68d799
cmtruit <christruitt@msn.com>
2017-10-29 21:30:25 -0500

    a

--- lop/api/urls.py
+++ lop/api/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import schema_view
+from .views import schema_view, include_docs_urls
 
 urlpatterns = [
     url(r'^api/$', schema_view),
+    url(r'^docs/', include_docs_urls(title='Lights-Out Patching', public=False))
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
