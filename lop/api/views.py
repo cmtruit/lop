6b7c66d103127c6a5bbcba7aa21e6fc41e3ef556
cmtruit <christruitt@msn.com>
2017-10-29 21:33:56 -0500

    a

--- lop/api/views.py
+++ lop/api/views.py
@@ -2,4 +2,4 @@
 from django.conf.urls import url
 from rest_framework_swagger.views import get_swagger_view
 from rest_framework.documentation import include_docs_urls
-schema_view = get_swagger_view(title='Lights-Out Patching API')
+schema_view = get_swagger_view(title='Lights-Out Patching API', url='/api')
