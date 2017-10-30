8b81be50fc620d3db6c2e5fe52812bcaaf13e1be
cmtruit <christruitt@msn.com>
2017-10-29 21:42:46 -0500

    updated views.py min

--- lop/api/views.py
+++ lop/api/views.py
@@ -3,22 +3,3 @@ from django.conf.urls import url
 from rest_framework_swagger.views import get_swagger_view
 from rest_framework.documentation import include_docs_urls
 schema_view = get_swagger_view(title='Lights-Out Patching API', url='/api')
-from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
-from rest_framework.schemas import SchemaGenerator
-from rest_framework.views import APIView
-from rest_framework_swagger import renderers
-
-
-class SwaggerSchemaView(APIView):
-    permission_classes = [AllowAny]
-    renderer_classes = [
-        renderers.OpenAPIRenderer,
-        renderers.SwaggerUIRenderer
-    ]
-
-    def get(self, request):
-        generator = SchemaGenerator()
-        schema = generator.get_schema(request=request)
-
-        return Response(schema_view)
