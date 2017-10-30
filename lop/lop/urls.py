0f03d3280aa87df8ba86fe5f8f3d0783a850810e
cmtruit <christruitt@msn.com>
2017-10-30 18:44:50 -0500

    adding import

--- lop/lop/urls.py
+++ lop/lop/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from . import views
 from django.conf import settings
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
