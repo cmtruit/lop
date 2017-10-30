53ac66d2330e3e1ff86bbfe3c044eb083ad5333b
cmtruit <christruitt@msn.com>
2017-10-29 20:59:35 -0500

    a

--- lop/api/views.py
+++ lop/api/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-from django.shortcuts import render
 from django.conf.urls import url
 from rest_framework_swagger.views import get_swagger_view
 
-schema_view = get_swagger_view(title='Lights-Out Patching API')
+schema_view = get_swagger_view(title='Pastebin API')
+
+urlpatterns = [
+    url(r'^$', schema_view)
+]
--- lop/lop/urls.py
+++ lop/lop/urls.py
@@ -22,6 +22,5 @@ from django.conf import settings
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
-    url(r'^api/', schema_view)
 
 ]
