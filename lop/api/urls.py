5ad224064170f06e72a90b45bd2539412e3c108e
cmtruit <christruitt@msn.com>
2017-10-29 21:22:04 -0500

    a

--- lop/api/urls.py
+++ lop/api/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import schema_view
 
-urlpatterns = {
-    url(r'^api/$', CreateView.as_view(), name="create"),
-}
+urlpatterns = [
+    url(r'^api/$', schema_view),
+]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
