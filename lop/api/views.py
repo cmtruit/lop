4b693b60177aced1741c26f2f1b1d690c40ce1f2
cmtruit <christruitt@msn.com>
2017-10-29 21:29:08 -0500

    a

--- lop/api/views.py
+++ lop/api/views.py
@@ -1,46 +1,5 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
 from rest_framework_swagger.views import get_swagger_view
-
+from rest_framework.documentation import include_docs_urls
 schema_view = get_swagger_view(title='Pastebin API')
-from rest_framework_swagger import exceptions
-from rest_framework_swagger.permissions import AllowAny
-from rest_framework_swagger.renderers import CoreJSONRenderer
-from rest_framework_swagger.response import Response
-from rest_framework_swagger.schemas import SchemaGenerator
-from rest_framework_swagger.views import APIView
-
-from . import renderers
-
-
-def get_swagger_view(title=None, url=None, patterns=None, urlconf=None):
-    """
-    Returns schema view which renders Swagger/OpenAPI.
-    """
-    class SwaggerSchemaView(APIView):
-        _ignore_model_permissions = True
-        exclude_from_schema = True
-        permission_classes = [AllowAny]
-        renderer_classes = [
-            CoreJSONRenderer,
-            renderers.OpenAPIRenderer,
-            renderers.SwaggerUIRenderer
-        ]
-
-        def get(self, request):
-            generator = SchemaGenerator(
-                title=title,
-                url=url,
-                patterns=patterns,
-                urlconf=urlconf
-            )
-            schema = generator.get_schema(request=request)
-
-            if not schema:
-                raise exceptions.ValidationError(
-                    'The schema generator did not return a schema Document'
-                )
-
-            return Response(schema)
-
-    return SwaggerSchemaView.as_view()
