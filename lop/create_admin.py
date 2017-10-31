133ba293754e180e9edc2aff1bd5003bdf2fb2c1
cmtruit <christruitt@msn.com>
2017-10-30 22:18:34 -0500

    a

--- lop/create_admin.py
+++ lop/create_admin.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 u = User.objects.get(username='admin')
 if not u:
     user = User.objects.create_user('admin', 'admin@lop.localhost.com', 'lightsout')
-    user.last_name = 'Lennon'
+    user.last_name = 'admin'
     user.save()
 else:
     print("User 'admin' Exists.. Nothing to do")
