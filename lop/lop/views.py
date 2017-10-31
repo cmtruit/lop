b2237592d91d35798e6d0e55f6cddaf6a4111cb9
cmtruit <christruitt@msn.com>
2017-10-30 19:16:51 -0500

    login required

--- lop/lop/views.py
+++ lop/lop/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.conf import settings
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def index(request):
     return render(request, 'dashboard.html')
