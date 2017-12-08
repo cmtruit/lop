6f076f8afb09ebeb397d6deaac2e6d8b8258ef98
cmtruit <christruitt@msn.com>
2017-12-08 10:55:53 -0500

    added local libs

---
+++ lop/lop/static/js/html5shiv.js
@@ -0,0 +1,14 @@
+<html>
+<head><title>404 Not Found</title></head>
+<body>
+<h1>404 Not Found</h1>
+<ul>
+<li>Code: NoSuchKey</li>
+<li>Message: The specified key does not exist.</li>
+<li>Key: libs/html5shiv/3.7.0/html5shiv.jshttps://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js</li>
+<li>RequestId: 2F20221B21A11BC9</li>
+<li>HostId: Mv5TB/PBv0qEM5ett9/rae8P8RBADGV/vcnbc8t9WIMEVYOypw9hXuDZb9j1AjC3E5+rwkyHRVw=</li>
+</ul>
+<hr/>
+</body>
+</html>
