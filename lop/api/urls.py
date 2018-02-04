fc937f05bd717ada01f7333fdf7b35c2f33b652d
cmtruit <christruitt@msn.com>
2018-02-04 10:52:08 -0500

    stuff

--- lop/api/urls.py
+++ lop/api/urls.py
@@ -4,7 +4,9 @@ from .views import schema_view, include_docs_urls
 
 urlpatterns = [
     url(r'^api/$', schema_view),
+    url(r'^api/v1$', schema_view),
     url(r'^docs/', include_docs_urls(title='Lights-Out Patching', public=False))
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
+
--- lop/api/views.py
+++ lop/api/views.py
@@ -2,4 +2,15 @@
 from django.conf.urls import url
 from rest_framework_swagger.views import get_swagger_view
 from rest_framework.documentation import include_docs_urls
-schema_view = get_swagger_view(title='Lights-Out Patching API', url='/api')
+from rest_framework_swagger.renderers import OpenAPIRenderer, SwaggerUIRenderer
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework import response, schemas
+import urls
+schema_view = get_swagger_view(title='Lights-Out Patching API')
+
+@api_view()
+@renderer_classes([SwaggerUIRenderer, OpenAPIRenderer])
+def schema_view(request):
+    generator = schemas.SchemaGenerator(title='Lights-Out Patching')
+    return response.Response(generator.get_schema())
+
