9ed3ee8d8ec9953535fb7afab12e683f1f47ef8e
Chris Truitt <christruitt@msn.com>
2018-05-14 20:49:22 +0000

    fixing tests

--- lop/lop/tests/tests.py
+++ lop/lop/tests/tests.py
@@ -1,6 +1,6 @@
 #tests.py
 from django.contrib.auth.models import User
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.test.client import Client
 import unittest
 
